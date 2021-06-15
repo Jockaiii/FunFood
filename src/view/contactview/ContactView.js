b6ab3617d8d9db1da1dc85c9ded27b47330ab060
00joaand <00jooand@gafe.molndal.se>
2021-06-15 11:05:19 +0200

    Preetis commit, uppdaterade contact vyn

--- src/view/contactview/ContactView.js
+++ src/view/contactview/ContactView.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 export const ContactView = () => {
+
+
+    const history = useHistory()
     return (
         <div>
-            <h1>This is the ContactView</h1>
+            <button> E mail </button>
+            <button> Mobile </button>
+
+
+            <button onClick={() => history.push('./HomeView')}> Go to Homepage</button>
         </div>
     )
 }
+
+export default ContactView
