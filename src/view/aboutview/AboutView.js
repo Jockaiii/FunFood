2c4e1e69fc492c593756e3e437f2ddfca425e686
mathilda serneblad <mathilda.serneblad@icloud.com>
2021-06-28 20:05:52 +0200

    gjorde några små ändringar i css

--- src/view/aboutview/AboutView.js
+++ src/view/aboutview/AboutView.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const AboutView = () => {
     return (
         <div>
-            A site for home chefs who need a little bit of insperation!
+            <h3>A site for home chefs who need a little bit of insperation!</h3>
         </div>
     )
 }
